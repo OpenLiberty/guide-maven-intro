5fabea3defc7d78e99610412dea665fd03c97b79
Alasdair Nottingham <notatibm@gmail.com>
2017-09-08 10:23:42 -0400

    Update copyright statement as per GitHub issue #2

--- finish/src/test/java/io/openliberty/guides/hello/it/EndpointIT.java
+++ finish/src/test/java/io/openliberty/guides/hello/it/EndpointIT.java
@@ -1,18 +1,15 @@
+// tag::comment[]
 /*******************************************************************************
- * (c) Copyright IBM Corporation 2017.
+ * Copyright (c) 2017 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
  *******************************************************************************/
+ // end::comment[]
 package io.openliberty.guides.hello.it;
 
 import static org.junit.Assert.*;
@@ -46,6 +43,6 @@ public class EndpointIT {
             assertTrue("Unexpected response body", response.contains("Hello! How are you today?"));
         } finally {
             method.releaseConnection();
-        }  
+        }
     }
 }
